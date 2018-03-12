d3b31a6f93c6c781262c5a66d1474cb8deb694b3
Luce Michaelis <lucerorocks17@gmail.com>
2018-03-12 22:30:30 +0000

    done in popcode

--- js/script.js
+++ js/script.js
@@ -3,7 +3,33 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
 $(document).ready(function(){
+  function giphyURLWithSearchTerm(searchTerm) {
   
+    
+    // write a function that will return a url for the giphy API with
+    // the searchTerm provided in the parameters
+}
+
+function appendImageToBody(srcURL) {
+    $("body").append('<img src="https://giphy.com/stickers/namslam-dogdings-3og0IJHMqlmPzy7sGs">');
+ 
+}
+
+function callGiphyAPIWithSearchTerm(searchTerm) {
+    $.ajax({
+      url: "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC",
+      method: "GET",
+      success: function(response) {
+          console.log(response);
+         console.log(response.data[4]);
+         var first = response.data[4] ;
+         console.log(first.type);
+           console.log(first.title);
+        
+      },
+    }); 
+}
+callGiphyAPIWithSearchTerm("dog");
   
   
   
